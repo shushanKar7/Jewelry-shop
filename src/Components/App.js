6d97e42498837030421efcf808ddb0cb4ffdb377
vilenhakobyan4444 <vladwebhakobyan@gmail.com>
2019-11-22 22:43:07 -0800

    install react-router-dom

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import "../index.css";
 import SlideShow from "./SlideShow/SlideShow";
+import SignIN from './Account/SignIn';
+import SignUp from './Account/SignUp';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <SlideShow />
-      </div>
+      <Router>
+        <div className="container">
+         <Route path = '/SignIn' component = {SignIN}></Route>
+         <Route path = '/SignUp' component = {SignUp}></Route>
+          <SlideShow />
+        </div>
+      </Router>
     );
   }
 }
