ee1273072ae1b6a67ca4c52bca02733f95d43796
Vladimir Hakobyan <Vladimir.Hakobyan@istc.com>
2019-11-25 18:23:54 +0400

    Validation for signIn

--- src/Components/Account/SignIn.js
+++ src/Components/Account/SignIn.js
@@ -1,11 +1,71 @@
 import React from "react";
 import { Fragment } from "react";
 import {Link} from 'react-router-dom';
+import { Component } from 'react';
 
-const SignIn = () => {
+const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+const validateForm = (errors) => {
+  let valid = true;
+  Object.values(errors).forEach(
+    (val) => val.length > 0 && (valid = false)
+  );
+  return valid;
+}
+
+const countErrors = (errors) => {
+  let count = 0;
+  Object.values(errors).forEach(
+    (val) => val.length > 0 && (count = count+1)
+  );
+  return count;
+}
+
+class SignIn extends Component {
+    state = {
+      formValid: false,
+      errorCount: null,
+      errors: {
+        username: '',
+        password: '',
+      }
+    }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    let errors = this.state.errors;
+
+    switch (name) {
+      case 'username': 
+        errors.username = 
+          value.length < 5
+            ? 'Username must be 5 characters long!'
+            : '';
+        break;
+      case 'password': 
+        errors.password = 
+          value.length < 8
+            ? 'Password must be 8 characters long!'
+            : '';
+        break;
+      default:
+        break;
+    }
+
+    this.setState({errors, [name]: value});
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({formValid: validateForm(this.state.errors)});
+    this.setState({errorCount: countErrors(this.state.errors)});
+  }
+
+  render() {
+    const {errors, formValid} = this.state;
     return (
-        <Fragment>
-            <div className="wrapper">
+      <Fragment>
+      <div className="wrapper">
         <div className="box1">
           <div className="content">
             <div className="registerLogo"></div>
@@ -14,7 +74,7 @@ const SignIn = () => {
         <div className="box2">
           <div className="contentSignIn">
             <h2 className="title">Sign In</h2>
-            <form>
+            <form onSubmit={this.handleSubmit} noValidate>
               <div>
                 <input
                   type="text"
@@ -22,7 +82,10 @@ const SignIn = () => {
                   name="username"
                   id="username"
                   placeholder="Enter Your Name"
+                  onChange={this.handleChange} noValidate
                 />
+                 {errors.username.length > 0 && 
+                  <span className='error'>{errors.username}</span>}
               </div>
               <div>
                 <input
@@ -31,7 +94,10 @@ const SignIn = () => {
                   name="password"
                   id="password"
                   placeholder="Enter Password"
+                  onChange={this.handleChange} noValidate
                 />
+                {errors.password.length > 0 && 
+                 <span className='error'>{errors.password}</span>}
               </div>
               <div>
                 <button className="submit">Sign In</button>
@@ -40,13 +106,19 @@ const SignIn = () => {
             <p>
               Don't have an account?{" "}
               <Link to = '/SignUp'><a>
-                Sign up
+                  Sign up
               </a></Link>{" "}
             </p>
           </div>
         </div>
       </div>
-        </Fragment>
-    );
-};
-export default SignIn;
+    </Fragment>
+    )
+  }
+}
+export default SignIn
+
+
+
+
+
