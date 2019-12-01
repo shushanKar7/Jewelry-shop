87fc2a9c4985cefb1a1fd95cc96a9f537e1cb649
vilenhakobyan4444 <vladwebhakobyan@gmail.com>
2019-12-01 04:39:07 -0800

    Add h3 in .overlayText

--- src/Components/ItemBox/ItemBox.js
+++ src/Components/ItemBox/ItemBox.js
@@ -5,6 +5,7 @@ function ItemBox(props) {
         <div className="itemBox">
             <div className="itemImage" style={{ backgroundImage: `url(${props.image})` }}>
                 <div className='overlayText .fadeIn-left'>
+                    <h3>This is a title</h3>
                     <p style={{ padding: '20px' }}>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                     </p>
