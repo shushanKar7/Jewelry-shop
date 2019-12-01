f4eba843256bb838c376a592d30b98e28ca17932
vilenhakobyan4444 <vladwebhakobyan@gmail.com>
2019-12-01 04:34:18 -0800

    Add hover effects for ItemImage

--- src/Components/ItemBox/ItemBox.js
+++ src/Components/ItemBox/ItemBox.js
@@ -4,15 +4,19 @@ function ItemBox(props) {
     return (
         <div className="itemBox">
             <div className="itemImage" style={{ backgroundImage: `url(${props.image})` }}>
-                <div className = 'overlayText'><span></span></div>
-                <div className = 'overlay'></div>
+                <div className='overlayText .fadeIn-left'>
+                    <p style={{ padding: '20px' }}>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+                </div>
+                <div className='overlay'></div>
             </div>
             <div className="itemDetail">
-                <div className = 'itemBoxSpan'>
+                <div className='itemBoxSpan'>
                     <span>{props.name}</span><br />
                     <span>{props.price}</span>
                 </div>
-                <div className = 'itemBoxButton'>
+                <div className='itemBoxButton'>
                     <button>ADD TO CART</button>
                     <button>BUY</button>
                 </div>
